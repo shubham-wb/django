807c73ee63c8b913dba5c12f0a3ae5f88ee6fd46
tensaidevx <shubham.gupta.npd@gmail.com>
2025-08-31 09:28:11 +0530

    'feature/templates'

--- base/views.py
+++ base/views.py
@@ -10,9 +10,17 @@ rooms = [
 ]
 
 
-
 def home(request):
-    return render(request,'home.html',{"rooms":rooms})
+    context = {"rooms":rooms}
+    return render(request,'base/home.html',context)
 
-def room(request):
-    return render(request,'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        
+        if i['id'] == int(pk):
+            room = i
+            print(room)
+    print('DEBUG room:', room)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
