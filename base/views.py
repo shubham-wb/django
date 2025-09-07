7ad9532ea7583d1b338074771f87a345007f8bdc
tensaidevx <shubham.gupta.npd@gmail.com>
2025-09-07 19:10:03 +0530

    'feature/message-crud'

--- base/views.py
+++ base/views.py
@@ -1,9 +1,10 @@
+
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from base.forms import RoomForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from django.contrib import  messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -90,7 +91,17 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
-    context = {'room': room, 'room_messages':room_messages}
+    participants = room.participants.all()
+
+    if request.method == "POST":
+        message = Message.objects.create(
+            user = request.user,
+            room = room, 
+            body = request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+    
+    context = {'room': room, 'room_messages':room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url="/login")
